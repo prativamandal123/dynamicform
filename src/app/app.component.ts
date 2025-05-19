2a75df8e9076f173a4761a9f8021a7a93ecd257a
prativamandal123 <mandalprativa66@gmail.com>
2025-05-19 12:23:51 +0545

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AutocompleteComponent } from './autocomplete/autocomplete.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AutocompleteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'dynamicform';
+ 
 }
---
+++ src/app/autocomplete/autocomplete.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-autocomplete',
+  imports: [],
+  templateUrl: './autocomplete.component.html',
+  styleUrl: './autocomplete.component.css'
+})
+export class AutocompleteComponent {
+
+}
