cc14956e7cbefb8cc64fd30e46c460609d51f13d
prativamandal123 <mandalprativa66@gmail.com>
2025-05-19 13:22:16 +0545

    push

--- src/app/autocomplete/autocomplete.component.ts
+++ src/app/autocomplete/autocomplete.component.ts
@@ -1,11 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPencil, faXmark } from '@fortawesome/free-solid-svg-icons'; 
+
 
 @Component({
   selector: 'app-autocomplete',
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule,FormsModule,FontAwesomeModule],
   templateUrl: './autocomplete.component.html',
   styleUrl: './autocomplete.component.css'
 })
 export class AutocompleteComponent {
+faXmark = faXmark; 
+faPencil = faPencil;
+  formModel = {
+    autocomplete: '',
+    required: false,
+    label: '',
+    helpText: '',
+    placeholder: '',
+    className: '',
+    name: '',
+    access: false,
+    roles: {
+      admin: false,
+      editor: false,
+      viewer: false,
+    }
+  };
+  onSubmit() {
+    console.log('Form submitted:', this.formModel);
+  }
 
 }
