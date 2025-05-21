418b78905706830c9b54b426bb074e94c7a8cc94
prativamandal123 <mandalprativa66@gmail.com>
2025-05-21 16:19:11 +0545

    pull

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,18 @@
-import { Component, ViewChild } from '@angular/core';
-import { CdkDragDrop, CdkDropList, DragDropModule } from '@angular/cdk/drag-drop';
+import { TextFieldComponent } from './../text-field/text-field.component';
+import { Component } from '@angular/core';
+import {  DragDropModule } from '@angular/cdk/drag-drop';
 import { AutocompleteComponent } from '../autocomplete/autocomplete.component';
 import { CommonModule } from '@angular/common';
 import { faPencil, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
-import { FormDataService } from '../form-data.service';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, DragDropModule,ReactiveFormsModule,FormsModule,FontAwesomeModule,HttpClientModule,AutocompleteComponent],
+  imports: [CommonModule, DragDropModule,ReactiveFormsModule,FormsModule,FontAwesomeModule,HttpClientModule,AutocompleteComponent,TextFieldComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
@@ -24,59 +24,20 @@ export class HomeComponent {
 
   formFields: { id: string; type: string; label: string;  value?: any; // Add this
  }[] = [];
-  selectedContent: string | null = null;
-
-
-
-
-  // onDropInRow(event: CdkDragDrop<any>) {
-  //   const draggedField = event.item.data;
-  //   if (!draggedField?.type) return;
-
-  //   const newField = {
-  //     id: crypto.randomUUID(),
-  //     type: draggedField.type,
-  //     label: draggedField.label,
-  //   };
-  //   this.formFields.splice(event.currentIndex, 0, newField);
-  // }
-
-  displayContent(contentName: string): void {
-    this.selectedContent = contentName;
-  }
-
+  
   trackByFieldPalette(index: number, item: { type: string }) {
     return item.type;
   }
 
-  // trackByFormField(index: number, item: { id: string }) {
-  //   return item.id;
-  // }
-//  faPencil = faPencil;
-//   faXmark = faXmark;
-//     showForm = false;
-//    formModel: any;
-  
- 
- 
-//    constructor(private formDataService: FormDataService ) {}
- 
-   
-//      ngOnInit(): void {
-//      this.formDataService.getFormModel().subscribe((data: any) => {
-//        // If the data is an array (like in your JSON), get the first item
-//        this.formModel = Array.isArray(data.formModel) ? data.formModel[0] : data.formModel;
-//      });
-//    }
-  
- 
-   // Flag to control visibility of additional form fields
- 
-   // Method to toggle visibility
-//    toggleForm(action: 'open' | 'close') {
-//      this.showForm = action === 'open';
-//    }
-//  onSubmit(){}
+  displayContent(field: any) {
+  const newField = {
+    id: crypto.randomUUID(),
+    type: field.type,
+    label: field.label,
+    value: '',
+  };
+  this.formFields.push(newField);
+}
  
 
 }
--- src/app/text-field/text-field.component.ts
+++ src/app/text-field/text-field.component.ts
@@ -1,24 +1,20 @@
+import { CdkDragDrop, DragDropModule } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faPencil, faXmark } from '@fortawesome/free-solid-svg-icons'; 
+import { faPencil, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FormDataService } from '../form-data.service';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import {CdkDragDrop, DragDropModule} from '@angular/cdk/drag-drop'
-
 
 @Component({
-  selector: 'app-autocomplete',
-    standalone: true,
-  imports: [CommonModule,ReactiveFormsModule,FormsModule,FontAwesomeModule,HttpClientModule,DragDropModule],
-  templateUrl: './autocomplete.component.html',
-  styleUrl: './autocomplete.component.css'
+  selector: 'app-text-field',
+ imports: [CommonModule,ReactiveFormsModule,FormsModule,FontAwesomeModule,HttpClientModule,DragDropModule],
+  templateUrl: './text-field.component.html',
+  styleUrl: './text-field.component.css'
 })
-export class AutocompleteComponent {
-  
-faPencil = faPencil;
+export class TextFieldComponent {
+  faPencil = faPencil;
  faXmark = faXmark;
    showForm = false;
   formModel: any;
@@ -69,5 +65,6 @@ onSubmit(){}
   trackByFormField(index: number, item: { id: string }) {
     return item.id;
   }
+
 
 }
